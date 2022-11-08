77988ae6f95f35ea65e0e005c1e83d87e81f6a31
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-08 11:12:28 +0000

    add new class Item and register model in admin.py

--- todo/models.py
+++ todo/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    done = models.BooleanField(null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False)   
