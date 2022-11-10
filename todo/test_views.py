343b223d9ad5c10523f9b693ebdf979a9c5f133b
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-10 10:30:58 +0000

    3rd test for test_views.py failed

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Item
 
 
 class TestDjango(TestCase):
@@ -12,3 +13,10 @@ class TestDjango(TestCase):
         response = self.client.get('/add')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/add_item.html')
+
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
+        
